133a9ba8cedf2ae047b13f28f25290fc1d3694cc
arnogeimer <arno.geimer@uni.lu>
2025-07-23 18:07:00 +0200

    formatting

--- flwr_contributions/shapley_values.py
+++ flwr_contributions/shapley_values.py
@@ -1,3 +1,4 @@
+import warnings
 from itertools import chain, combinations
 from typing import Callable, List, Tuple, Union
 
@@ -5,15 +6,16 @@ import numpy as np
 from flwr.common import FitRes, parameters_to_ndarrays
 from flwr.server.client_proxy import ClientProxy
 from flwr.server.strategy import FedAvg
-import warnings
 
 """ Samplers are implementations of https://arxiv.org/abs/2104.12199 """
 """ Using Samplers in FL has been proposed in https://arxiv.org/pdf/2109.02053 """
 
+
 def int_to_set(x: int, results: List[int]) -> tuple[int]:
-    bin = format(x, 'b').rjust(len(results), '0')
+    bin = format(x, "b").rjust(len(results), "0")
     sample = tuple([results[i] for i, entry in enumerate(bin) if entry == "1"])
     return sample
+
 
 class Sampler(object):
     def __init__(self, results: List[int]) -> None:
@@ -45,19 +47,27 @@ class FullSampler(Sampler):
         super().__init__(results)
 
     def generate_samples(self):
-        warnings.warn("""Warning: It is not recommended to use the full sampler for high amount of clients.
-                         Due to exponential growth of powerset sizes, your system may kill the process.""")
-        self.samples = list(chain.from_iterable(combinations(self.results, r) for r in range(1, len(self.results) + 1)))
+        warnings.warn(
+            """Warning: It is not recommended to use the full sampler for high amount of clients.
+                         Due to exponential growth of powerset sizes, your system may kill the process."""
+        )
+        self.samples = list(
+            chain.from_iterable(
+                combinations(self.results, r) for r in range(1, len(self.results) + 1)
+            )
+        )
 
 
 class LeaveOneOutSampler(Sampler):
     # Leave-one-out
-    
+
     def __init__(self, results: List[int], **kwargs):
         super().__init__(results)
 
     def generate_samples(self):
-        self.samples = sorted([[j for j in self.results if j != i] for i in self.results] + [self.results])
+        self.samples = sorted(
+            [[j for j in self.results if j != i] for i in self.results] + [self.results]
+        )
 
 
 class MonteCarloSampler(Sampler):
@@ -71,7 +81,10 @@ class MonteCarloSampler(Sampler):
     def generate_monte_carlo_samples(self):
         # Due to the computational complexity of power sets, we uniformly sample powerset indices and fit the subset through the binary representation
         np.random.seed(self.seed)
-        self.samples = [int_to_set(entry, self.results) for entry in np.random.randint(0, 2**len(self.results), self.samplesize)]
+        self.samples = [
+            int_to_set(entry, self.results)
+            for entry in np.random.randint(0, 2 ** len(self.results), self.samplesize)
+        ]
 
     def generate_samples(self):
         self.generate_monte_carlo_samples()
@@ -142,19 +155,31 @@ class StratifiedSampler(Sampler):
 
     def __init__(self, results: List[int], **kwargs):
         super().__init__(results)
+
         # We search for the optimal split to account for the samplesize the user specifies.
         # By splitting into smaller and smaller subsets, we decrease the total amount of samples.
         # With find_best_split_size, we search for the amount of subsets which is just below the desired sample size.
         # Note that, for too small sample sizes (below len(results)), we have to ignore "sample_number", as a stratified sampling would be impossible.
         def find_best_split_size():
             k = len(results)
-            sizes = np.array([(i - (k%i))*(2**(int(k / i))) + (k % i)*(2**(int(k / i) + 1)) for i in range(1, len(results))])
+            sizes = np.array(
+                [
+                    (i - (k % i)) * (2 ** (int(k / i)))
+                    + (k % i) * (2 ** (int(k / i) + 1))
+                    for i in range(1, len(results))
+                ]
+            )
             samplesize = kwargs.get("sample_number", 10)
             try:
                 best_i = min(np.flatnonzero(sizes < samplesize)) + 1
             except Exception:
                 best_i = k
-            return np.cumsum([0] + (best_i - (k % best_i))*[int(k / best_i)] + (k % best_i) * [int(k / best_i) + 1])
+            return np.cumsum(
+                [0]
+                + (best_i - (k % best_i)) * [int(k / best_i)]
+                + (k % best_i) * [int(k / best_i) + 1]
+            )
+
         self.split_sets = find_best_split_size()
         self.seed = kwargs.get("seed", 1)
 
@@ -166,9 +191,16 @@ class StratifiedSampler(Sampler):
             self.results[self.split_sets[i] : self.split_sets[i + 1]]
             for i in range(0, len(self.split_sets) - 1)
         ]
-        samples = [list(chain.from_iterable(combinations(entry, r) for r in range(1, len(entry) + 1))) for entry in splits]
+        samples = [
+            list(
+                chain.from_iterable(
+                    combinations(entry, r) for r in range(1, len(entry) + 1)
+                )
+            )
+            for entry in splits
+        ]
         self.samples = [tuple(set(x)) for subset in samples for x in subset]
-    
+
     def generate_samples(self):
         self.generate_stratified_samples()
         self.generate_test_samples()
@@ -183,7 +215,6 @@ def multi_round_reconstruction(
     sampler: Sampler = FullSampler,
     sample_ratio: float = 0.5,
 ) -> Tuple[int, Tuple[int], float, float]:
-
     """
     A function calculating multi-round Shapley-value contributions, based on results sent to the server (source: https://arxiv.org/pdf/1909.08525).
     Is plugged into aggregate_fit in the flwr.strategy.
@@ -211,6 +242,7 @@ def multi_round_reconstruction(
         shapley_dict.append([subset, loss, metrics["accuracy"]])
     return shapley_dict
 
+
 def one_round_reconstruction(
     server_round: int,
     results: List[Tuple[ClientProxy, FitRes]],
@@ -218,10 +250,9 @@ def one_round_reconstruction(
     evaluate_fn: Callable,
     aggregate_fit: Callable = FedAvg.aggregate_fit,
     samples: List[List[int]] = [],
-    initial_parameters = None,
+    initial_parameters=None,
     final_round: int = 0,
 ) -> Tuple[int, Tuple[int], float, float]:
-
     """
     A function calculating one-round Shapley-value contributions, based on results sent to the server (source: https://arxiv.org/pdf/1909.08525).
     Is plugged into aggregate_fit in the flwr.strategy.
@@ -252,7 +283,7 @@ def one_round_reconstruction(
 
     if server_round == final_round:
         shapley_dict: List = []
-        for (subset, gradient) in zip(samples, gradients):
+        for subset, gradient in zip(samples, gradients):
             final_model = parameters_to_ndarrays(init_params - gradient)
             loss, metrics = evaluate_fn(server_round, final_model, {})
             shapley_dict.append([subset, loss, metrics["accuracy"]])
